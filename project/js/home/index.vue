3cab19fe194960cbf9f424cc527dfb931bc4b350
1023mo <d1024WEI@163.com>
2019-07-07 16:37:30 +0800

    add template

---
+++ project/js/home/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>reduce高级用法</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      names: ['lisa', 'Emma', 'sophia'],
+      countObj: findCount()
+    }
+  },
+  methods: {
+    findCount () {
+      this.names.reduce((pre, cur) => {
+        if (cur in pre) {
+          pre[cur]++
+        } else {
+          pre[cur] = 1
+        }
+        return pre
+      }, {})
+    }
+  }
+}
+</script>
